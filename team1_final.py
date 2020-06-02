907d8d7bb7bbab38f03fd62219a11da09db3e133
SpearceCSUMB <44475801+SpearceCSUMB@users.noreply.github.com>
2020-06-02 15:57:04 -0700

    Add files via upload

--- team1_final.py
+++ team1_final.py
@@ -4,7 +4,7 @@
 
 import os
 from flask import Flask
-app = Flask(escape)
+app = Flask(__name__)
 
 
 '''
@@ -644,5 +644,6 @@ repaint(mainWindow)
 @app.route("/")
 play()
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
